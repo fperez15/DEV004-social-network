89737c5be0bcec5b15ea915a16046af33d246795
Irene Martínez <marttre20@gmail.com>
2023-02-23 14:08:05 -0600

    Agrega formulario de registro con estilo y conexión con firebase

--- src/components/register.js
+++ src/components/register.js
@@ -1,13 +1,15 @@
 export const register = () => {
   const registerSection = document.createElement("section");
-  const inpName = document.createElement("input");
-  const inpPassword = document.createElement("input");
-  const inpEmail = document.createElement("input");
-
-  registerSection.appendChild(inpName);
-  registerSection.appendChild(inpPassword);
-  registerSection.appendChild(inpEmail);
-
+  registerSection.innerHTML = `<section class="regSection">
+  <img class= "logo" src="./img/logo.png">
+  <form class="formRegister">
+  <input class="form" id="name" type="text" placeholder="Name"></input>
+  <input class="form" id="email" type="email" placeholder="Email"></input>
+  <input class="form" id="birthday" type="date"></input>
+  <input class="form" id="password" type="password" placeholder="Pasword"></input>
+  <button class="btnRegister" id="btnRegister">REGISTER</button>
+  </form>
+  </section>
+  `;
   return registerSection;
-  console.log(registerSection);
 };
